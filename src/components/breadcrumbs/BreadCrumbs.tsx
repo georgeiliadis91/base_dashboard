a2ae409d41d0b76429f8f0e42e81f7194fa2a9d1
georgeiliadis91 <george.iliadis.91@gmail.com>
2021-11-03 23:19:38 +0200

    copilot breadcrumbs

---
+++ src/components/breadcrumbs/BreadCrumbs.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useLocation } from 'react-router'
+import styles from './BreadCrumbs.module.css'
+interface Props {
+    
+}
+
+
+//BreadCrumbs Component
+export const BreadCrumbs: React.FC<Props> = () => {
+    const routes = useLocation();
+    
+    //Get the pathname from the location object
+    const breadcrumbs = routes.pathname.split('/').filter(x => x);
+
+    //Capitalise first letter of each word
+    const capitalise = (str: string) => {
+        return str.replace(/\b\w/g, l => l.toUpperCase());
+    }
+
+    return (
+        <div className={styles.breadcrumbs_container}>
+            {breadcrumbs.map((crumb, index) => {
+                return <span className={styles.breadcrumbs}>{capitalise(crumb)} {index !== breadcrumbs.length-1 && `-`}</span>})}
+        </div>
+    )
+}
--- src/layouts/dasboard/components/TopBar/TopBar.tsx
+++ src/layouts/dasboard/components/TopBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BreadCrumbs } from "../../../../components/breadcrumbs/BreadCrumbs";
 import { DropDown } from "../../../../components/dropdown/DropDown";
 import styles from "./TopBar.module.css";
 interface Props {}
@@ -7,7 +8,7 @@ export const TopBar = (props: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.breadcrumbs_container}>
-        bread / crumbs / go / here
+        <BreadCrumbs/>
       </div>
       <div className={styles.user_menu_container}>
         <DropDown menuTitle="userName" menuElements={MenuList} />
