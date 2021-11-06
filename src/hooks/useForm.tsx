1d764b1bc5292f0594e1f5734b7b460fa9bcfc2c
iliadis <iliadis@intralot.com>
2021-11-06 12:03:01 +0200

    basic login for mand form functionality hook

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+
+interface IFormfields{
+    name?:string;
+    password?:string;
+}
+
+
+
+//custom hook with form values 
+export function useForm(initialValues : IFormfields){
+    const [formValues, setFormValues] = useState<IFormfields>(initialValues);
+    const [formErrors, setFormErrors] = useState<IFormfields>({});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const errors = validate(formValues);
+        setFormErrors(errors);
+    };
+
+    return {
+        formValues,
+        formErrors,
+        handleChange,
+        handleSubmit
+    }
+}
+
+
+//validates form function
+function validate(formValues: IFormfields){
+    const errors: IFormfields|null = {};
+    if(!formValues.name){
+        errors.name = 'Name is required';
+    }
+    if(!formValues.password){
+        errors.password = 'Password is required';
+    }
+    return errors;
+}
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
+import styles from './Login.module.css';
 
 interface Props {}
 
+const defaultValues ={
+  name:'',
+  password:''
+}
+
 export const Login = (props: Props) => {
-  return <div>Login View</div>;
+  const { formValues, formErrors, handleChange, handleSubmit} =useForm(defaultValues);
+
+
+  return (
+      <div className={styles.login_page_container}>
+        <div className={styles.login_form_container}>
+          <span>{JSON.stringify(formErrors)}</span>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input name="name" type="email" onChange={handleChange} value={formValues.name}/> 
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password" onChange={handleChange} value={formValues.password}/> 
+            <input value="submit" type="submit"/> 
+          </form>
+        </div>
+      </div>
+      );
 };
