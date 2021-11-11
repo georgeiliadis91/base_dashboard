8cb8405c9ca9a6faad09df2de1235a37c9ba66a9
georgeiliadis91 <george.iliadis.91@gmail.com>
2021-11-12 00:30:31 +0200

    removed trasah

--- src/layouts/dasboard/components/TopBar/TopBar.tsx
+++ src/layouts/dasboard/components/TopBar/TopBar.tsx
@@ -10,7 +10,7 @@ export const TopBar = (props: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.breadcrumbs_container}>
-        <BreadCrumbs/>login
+        <BreadCrumbs/>
       </div>
       <div>
         <span>
