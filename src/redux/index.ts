09072c51092a5da76dac9ca5b076eba80251ac0f
georgeiliadis91 <george.iliadis.91@gmail.com>
2022-01-17 19:30:11 +0200

    comment

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -3,6 +3,6 @@ import { AppDispatch, AppState } from "./reducers";
 
 export { allReducer as globalStore } from "./reducers";
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// User in this way to avoid redeclaration of the same type
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
